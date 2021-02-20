6c47420944e9d6b69c9bbbbd32dcbd2b31a6784d
ngnawensv <ngnawens@gamil.com>
2021-02-20 10:21:41 +0100

    Integration of springdoc-openapi

--- service-departement/src/main/java/cm/belrose/servicedepartement/ServiceDepartementApplication.java
+++ service-departement/src/main/java/cm/belrose/servicedepartement/ServiceDepartementApplication.java
@@ -1,11 +1,13 @@
 package cm.belrose.servicedepartement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@OpenAPIDefinition
 public class ServiceDepartementApplication {
 
 	public static void main(String[] args) {
--- service-departement/src/main/java/cm/belrose/servicedepartement/controller/DepartementController.java
+++ service-departement/src/main/java/cm/belrose/servicedepartement/controller/DepartementController.java
@@ -2,6 +2,10 @@ package cm.belrose.servicedepartement.controller;
 
 import cm.belrose.servicedepartement.model.Departement;
 import cm.belrose.servicedepartement.service.DepartementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +18,31 @@ public class DepartementController {
     @Autowired
     private DepartementService departementService;
 
+    @Operation(summary = "Enregistrer un département dans la  la base de données")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Enregistrement avec succès!!!",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Echec d'enregistrement!!!",
+                    content = @Content)
+    })
+
     @PostMapping("/")
     public Departement saveDepartement( @RequestBody Departement departement){
         log.info("inside the saveDepartement of DepartementController");
         return departementService.saveDepartement(departement);
     }
+
+    @Operation(summary = "Recherche le département {} dans la  la base de données")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Rechercher un département dans la base de donnée",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Département introuvable",
+                    content = @Content)
+    })
 
     @GetMapping("{id}")
     public Departement findDepartemetById(@PathVariable("id") Long id){
